65c93067a6897b85fa247dd33d28b41b1cca51bd
aymericCOUPRIE <43545632+aymericCOUPRIE@users.noreply.github.com>
2021-03-10 22:50:25 +0100

    Add files via upload

---
+++ mal_projetwebmobile_frontmobile/mal_projetwebmobile_frontmobile/mal_projetwebmobile_frontmobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  mal_projetwebmobile_frontmobileApp.swift
+//  mal_projetwebmobile_frontmobile
+//
+//  Created by user190207 on 3/10/21.
+//
+
+import SwiftUI
+
+@main
+struct mal_projetwebmobile_frontmobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
